894b6610bb469900f362d114ca061b34523a5a9b
4562sky <4562sky@naver.com>
2022-09-27 18:01:29 +0900

    DOM

--- 01/index.js
+++ 01/index.js
@@ -14,3 +14,75 @@ const funPrint = (n) => {
   console.log(`버튼${n}`);
 }
 
+//변수
+//console.log(x);
+let x = 100;
+x = 100 *2;
+console.log(x);
+
+//DOM이 생성이 되고 난 후에 요소룰 CRUD해야함.
+
+document.addEventListener("DOMContentLoaded",() =>{
+  //console.log(document.getElementById("bt").innerHTML);
+
+  /* 요소생성
+ let bt = document.createElement("button")
+docunebt.getElementById("bt").append(bt)
+*/
+
+  //요소 찾기
+  //결과가 HTML Collection
+  //const bts1 = document.getElementsByClassName(".bt1");
+  //console.log(bts1);
+  //결과가 HTML Collection
+  //const bts2 = document.getElementsByName("button");
+  //console.log(bts2);
+  //결과가 문자열
+  //const bts3 = document.querySelector(".bt1");
+  //console.log(bts3);
+  //결과가 NodeList
+  const bts4 = document.querySelectorAll(".bt1");
+ /*
+  console.log(bt4);
+
+  console.log(bts4[0].innerHTML);
+  console.log(bts4[0].innerText);
+  console.log(bts5[1].textContent);
+*/
+
+  /*반복문
+  for(let i = 0; i < bts4.length ; i++){
+    console.log(typeof(bts4[i]));
+    bts4[i].innerHTML = `mybutton${i+1}`
+    console.log(bts4[i].innerHTML);
+  }
+  */
+  /*반복문 folr in ==>>>> 키 값만 가지고 온다!!!!!!!
+  for(let i in bts4){
+    console.log(i)
+  }
+*/
+  /*반복문 foreach
+  bts4.forEach((item) => {
+console.log(item);
+
+  });
+
+  bts7.forEach((item) => {
+    console.log(`${idx} : ${item.innerHTML}`);
+  });
+*/
+
+  /*반복문 for ...of*/
+    for(let item of bts4){
+      console.log(item.innerHTML);
+      }
+
+    for(let [idx, item] of bts4.entries()) {
+      console.log(`${idx} : ${item.innerHTML}`);
+      break;
+    }
+
+
+
+} );
